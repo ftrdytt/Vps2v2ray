e1ba1ec9e984b6a8a736e5a773c470dabf15d580
Affuyfuffyt <98175855+Affuyfuffyt@users.noreply.github.com>
2026-07-15 05:02:56 +0300

    Update StoryUploadActivity.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/StoryUploadActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/StoryUploadActivity.kt
@@ -11,6 +11,7 @@ import android.util.Base64
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
